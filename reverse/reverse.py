d9441261b36e4e6ba9b65051f9c1b201cd75143d
Janessa Garrow <janessa.garrow@gmail.com>
2020-09-26 10:45:33 -0600

    Complete reverse

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,38 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # if there's nothing in the list
+        if self.head is None:
+            return None
+        
+        # if there's only 1 thing in the list
+        elif self.head.next_node is None:
+            return None
+        
+        # if there's 2+ things in the list
+        elif self.head.next_node:
+            # loop through nodes
+            # for each node, set next_node.next_node to the previous node
+            prev_node = None
+            curr_node = self.head
+            # next_node = self.head.next_node
+
+            # while we're not at the end of the list/at the tail
+            while curr_node is not None:
+                # hold onto the next node
+                new_next = curr_node.next_node
+
+                # update the next node to be the current prev_node (flip next to point to the previous)
+                curr_node.next_node = prev_node
+
+                # update the prev node to be the current node
+                prev_node = curr_node
+
+                # update the current node to be the next node
+                curr_node = new_next
+            
+            # curr_node will be None if we finished our loop 
+            # prev_node will be the new head (the original tail)
+            if curr_node is None:
+                self.head = prev_node
+
