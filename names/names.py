c7f9017c2948c384b2798a33cf1fd20165a2942c
Janessa Garrow <janessa.garrow@gmail.com>
2020-09-26 11:36:51 -0600

    Complete stretch using python set and intersection methods

--- names/names.py
+++ names/names.py
@@ -19,48 +19,50 @@ duplicates = []  # Return the list of duplicates in this data structure
 #             duplicates.append(name_1)
 
 
-
+# binary search -- run time is <0.07 seconds
 # assumes name_list is sorted
-def binary_search(name_list, list_len, value_to_compare):
-    low = 0 # lowest index we're looking at
-    high = list_len - 1 # highest index we're looking at
+# def binary_search(name_list, list_len, value_to_compare):
+#     lowest_index = 0 # lowest index we're looking at
+#     highest_index = list_len - 1 # highest index we're looking at
 
-    while low <= high:
-        # print('\nlow: ', low)
-        # print('high: ', high)
-        # middle index between the current low and high
-        mid = int(low + ((high - low) / 2))
-        # print('mid: ', mid)
+#     while lowest_index <= highest_index:
+#         # middle index between the current lowest_index and high
+#         middle_index = int(lowest_index + ((highest_index - lowest_index) / 2))
 
-        # if we found a duplicate, return True
-        if name_list[mid] == value_to_compare:
-            return True
+#         # if we found a duplicate, return True
+#         if name_list[middle_index] == value_to_compare:
+#             return True
 
-        # else if the middle value too big, make high 1 smaller than the middle index
-        elif name_list[mid] > value_to_compare:
-            high = mid - 1
+#         # else if the middle value too big, make highest_index 1 smaller than the middle index
+#         elif name_list[middle_index] > value_to_compare:
+#             highest_index = middle_index - 1
         
-        # else if the middle value is too small, make low 1 bigger than the middle index
-        else:
-            low = mid + 1
+#         # else if the middle value is too small, make lowest_index 1 bigger than the middle index
+#         else:
+#             lowest_index = middle_index + 1
     
-    # return False if we never find a match
-    return False
+#     # return False if we never find a match
+#     return False
 
 
-sorted_names_1 = sorted(names_1)
+# sorted_names_1 = sorted(names_1)
 
 
-for name_2 in names_2:
-    if binary_search(sorted_names_1, len(sorted_names_1), name_2) is True:
-        duplicates.append(name_2)
+# for name_2 in names_2:
+#     if binary_search(sorted_names_1, len(sorted_names_1), name_2) is True:
+#         duplicates.append(name_2)
     
-
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# run time is <0.005 seconds
+# duplicates = sorted(list(set(names_1).intersection(names_2)))
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+
